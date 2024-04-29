8b116b2050bc3e2dc83c140e7f89685876f2a459
Timo S. <164857882+tiscit@users.noreply.github.com>
2024-04-29 11:02:03 +0000

    Updated layout and design of app.vue

--- lab-24-vs/src/App.vue
+++ lab-24-vs/src/App.vue
@@ -1,8 +1,10 @@
 <template>
- <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App (HSE-LAB Heftig krasser text)"/>
-    <div>
+  <div id="app">
+    <header>
+      <img class="logo-hse" alt="Hse logo" src="./assets/hse.svg">
+      <h1>Lab - Parallel and Distributed Systems</h1>
+    </header>
+    <div class="container">
       <table>
         <thead>
           <tr>
@@ -15,31 +17,32 @@
           </tr>
         </tbody>
       </table>
-      <form @submit.prevent="addTodo">
+      <div class="input-buttons">
         <input v-model="newTodo" type="text" placeholder="New ToDo">
-        <button type="submit">Add</button>
-      </form>
-      <button @click="deleteTodo">Delete</button>
+        <button class="add-btn" type="submit" @click="addTodo"><i class="fas fa-plus"></i> Add</button>
+        <button class="delete-btn" @click="deleteTodo"><i class="fas fa-trash"></i> Delete</button>
+      </div>
     </div>
- </div>
+    <footer>
+      <img class="logo-vue" alt="Vue logo" src="./assets/logo.png">
+      <p font-size="14">SS24 - Team: Damaris Kröner & Timo Schaller</p>
+    </footer>
+  </div>
 </template>
 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
- name: 'App',
- components: {
-    HelloWorld
- },
- data() {
+  name: 'App',
+  components: {
+  },
+  data() {
     return {
       todos: ['First ToDo', 'Second ToDo'], // Initial ToDo list
       newTodo: '' // Placeholder for new ToDo input
     }
- },
- methods: {
+  },
+  methods: {
     addTodo() {
       if (this.newTodo) {
         this.todos.push(this.newTodo);
@@ -47,62 +50,118 @@ export default {
       }
     },
     deleteTodo() {
-      if (this.todos.length > 0) {
-        this.todos.pop();
+      const index = this.todos.indexOf(this.newTodo);
+      if (index > -1) {
+          this.todos.splice(index, 1);
+          this.newTodo = ''; // Clear the input field after deletion
       }
     }
- }
+  }
 }
 </script>
 
-
 <style>
 #app {
- font-family: Avenir, Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
- -moz-osx-font-smoothing: grayscale;
- text-align: center;
- color: #2c3e50;
- margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  max-width: 800px;
+  /* Limit the width of the app */
+}
+
+header,
+footer {
+  background-color: #f8f9fa;
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: nowrap;
+}
+
+header h1 {
+  font-size: 25px;
+  margin: 0;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 table {
- width: 100%;
- border-collapse: collapse;
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 20px;
+  background-color: white;
 }
 
-th, td {
- border: 1px solid #ddd;
- padding: 8px;
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
 }
 
 th {
- padding-top: 12px;
- padding-bottom: 12px;
- text-align: left;
- background-color: #4CAF50;
- color: white;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #35495e;
+  color: white;
+}
+
+.input-buttons {
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  margin-bottom: 20px;
 }
 
 input[type="text"] {
- width: 100%;
- padding: 12px 20px;
- margin: 8px 0;
- box-sizing: border-box;
+  flex-grow: 1;
+  padding: 12px 20px;
+  margin: 8px 0;
+  margin-right: 10px;
+  box-sizing: border-box;
 }
 
 button {
- background-color: #4CAF50;
- color: white;
- padding: 14px 20px;
- margin: 8px 0;
- border: none;
- cursor: pointer;
- width: 100%;
+  background-color: #41b883;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  margin-left: 10px;
+  border: none;
+  cursor: pointer;
 }
 
 button:hover {
- opacity: 0.8;
+  opacity: 0.75;
+}
+
+.add-btn {
+  background-color: #4caf50;
+}
+
+.delete-btn {
+  background-color: #a00120;
+}
+
+.logo-vue {
+  width: 20px;
+  height: auto;
+  margin-right: 10px;
+}
+
+.logo-hse{
+  width: 80px;
+  height: auto;
+  margin-right: 50px;
 }
 </style>
-
